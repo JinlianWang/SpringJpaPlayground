7664f8e7184be289c1eb9753dc54b2631fb58c46
Sunny Wang <wangjinlian@hotmail.com>
2018-04-08 15:49:58 -0400

    Workaround to the issue that converter cannot use conversionSerice by putting a conversion helper method in service class.

--- src/main/java/com/springjpa/service/CustomerServiceImpl.java
+++ src/main/java/com/springjpa/service/CustomerServiceImpl.java
@@ -1,5 +1,7 @@
 package com.springjpa.service;
 
+import java.util.HashSet;
+
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,7 +9,9 @@ import org.springframework.core.convert.ConversionService;
 import org.springframework.stereotype.Service;
 
 import com.springjpa.model.db.CustomerDbEntity;
+import com.springjpa.model.db.RoleDbEntity;
 import com.springjpa.model.http.CustomerResponse;
+import com.springjpa.model.http.RoleResponse;
 import com.springjpa.repo.CustomerRepository;
 
 @Service
@@ -23,7 +27,17 @@ public class CustomerServiceImpl implements CustomerService {
 	@Transactional
 	public CustomerResponse locateCustomer(String id) {
 		CustomerDbEntity one = repository.findOne(id);
+		CustomerResponse response = convertToCustomerResponse(one);
+		return response;
+	}
+
+	private CustomerResponse convertToCustomerResponse(CustomerDbEntity one) {
 		CustomerResponse response = conversionService.convert(one, CustomerResponse.class);
+		HashSet<RoleResponse> set = new HashSet<RoleResponse>();
+		for(RoleDbEntity role : one.getRoles()) {
+			set.add(conversionService.convert(role, RoleResponse.class));
+		}
+		response.setRoles(set);
 		return response;
 	}
 
