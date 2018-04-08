c9e143997781c7e9d0f9059e246cd40e7210d254
Sunny Wang <wangjinlian@hotmail.com>
2018-04-08 15:32:13 -0400

    Use custom xml element name.

--- src/main/java/com/springjpa/model/http/CustomerResponse.java
+++ src/main/java/com/springjpa/model/http/CustomerResponse.java
@@ -7,7 +7,7 @@ import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
-@XmlRootElement
+@XmlRootElement(name="customer")
 @XmlAccessorType(XmlAccessType.FIELD)
 public class CustomerResponse {
 
